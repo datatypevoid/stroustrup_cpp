0862a11ab4d5d3bca39b4671e80e701de4e0e11d
David Newman <david.r.niciforovic@gmail.com>
2016-09-26 10:54:54 -0700

    style: format identations to two characters

--- 04/char_for_loop/main.cpp
+++ 04/char_for_loop/main.cpp
@@ -5,31 +5,31 @@
 
 int main() {
 
-    constexpr int alphabet_length = 25;
+  constexpr int alphabet_length = 25;
 
-    constexpr int lowercase_starting_index = 0;
+  constexpr int lowercase_starting_index = 0;
 
-    constexpr int uppercase_starting_index = 224;
+  constexpr int uppercase_starting_index = 224;
 
-    std::cout << "\nPrinting lowercase characters [a - z] : \n"
-              << std::endl;
+  std::cout << "\nPrinting lowercase characters [a - z] : \n"
+            << std::endl;
 
-    for(int i = lowercase_starting_index; i <= alphabet_length; i++) {
-         std::cout << char('a' + i) << "\t" << i << std::endl;
-    }
+  for(int i = lowercase_starting_index; i <= alphabet_length; i++) {
+    std::cout << char('a' + i) << "\t" << i << std::endl;
+  }
 
-    std::cout << "\nPrinting uppercase characters [A - Z] : \n"
-              << std::endl;
+  std::cout << "\nPrinting uppercase characters [A - Z] : \n"
+            << std::endl;
 
-    for(int i = uppercase_starting_index;
-            i <= uppercase_starting_index + alphabet_length;
-            i++) {
-        std::cout << char('a' + i) << "\t" << i << std::endl;
-    }
+  for(int i = uppercase_starting_index;
+          i <= uppercase_starting_index + alphabet_length;
+          i++) {
+    std::cout << char('a' + i) << "\t" << i << std::endl;
+  }
 
-    std::cout << "\nPrinting standard digits [0 - 9] : \n" << std::endl;
+  std::cout << "\nPrinting standard digits [0 - 9] : \n" << std::endl;
 
-    for(int i = 207; i <= 216; i++) {
-        std::cout << char('a' + i) << "\t" << i << std::endl;
-    }
+  for(int i = 207; i <= 216; i++) {
+    std::cout << char('a' + i) << "\t" << i << std::endl;
+  }
 }

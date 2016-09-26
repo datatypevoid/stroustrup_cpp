1da65435517b1aa4d3e6aa2a4a78a84f936dceaa
David Newman <david.r.niciforovic@gmail.com>
2016-09-26 15:20:43 -0700

    feat: check percentage diff
    * If the inputs differ by one percent or less, display an alert

--- 04/drill/main.cpp
+++ 04/drill/main.cpp
@@ -1,5 +1,8 @@
+#include <cmath>
 #include <iostream>
 #include <vector>
+
+double calculate_percentage_difference(double x, double y);
 
 int main() {
 
@@ -8,6 +11,8 @@ int main() {
 
   double input_01;
   double input_02;
+
+  double constexpr percent_difference_threshold = 1;
 
   while((std::cin >> input_01 && std::cin >> input_02)
             && (input_01 != '|') && input_02 != '|') {
@@ -21,6 +26,22 @@ int main() {
       std::cout << "The value " << input_01 << " is equal to value "
                 << input_02 << std::endl;
     }
+    if(calculate_percentage_difference(input_01, input_02)
+         <= percent_difference_threshold) {
+      std::cout << "The numbers are within one percent of each other"
+                << std::endl;
+    }
   }
+  
   return 0;
+}
+
+/**
+ * Calculates the difference between two inputs
+ * @param x - numeric input of type `double`
+ * @param y - numeric input of type `double`
+ * @return double - percentage difference between `x` and `y`
+ */
+double calculate_percentage_difference(double x, double y) {
+  return (std::abs(x - y) / ((x + y) / 2)) * 100;
 }

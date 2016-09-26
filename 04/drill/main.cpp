1462c13318756e6013273659ec9715d995167a5c
David Newman <david.r.niciforovic@gmail.com>
2016-09-26 14:36:42 -0700

    feat: complete part two of the drill exercise

--- 04/drill/main.cpp
+++ 04/drill/main.cpp
@@ -12,8 +12,13 @@ int main() {
   while((std::cin >> input_01 && std::cin >> input_02)
             && (input_01 != std::string("|")
                 && input_02 != std::string("|"))) {
-    std::cout << "Input 1: " << input_01 << std::endl;
-    std::cout << "Input 2: " << input_02 << std::endl;
+    if(input_01 > input_02) {
+      std::cout << "The smaller value is: " << input_02 << std::endl;
+      std::cout << "The larger value is: " << input_01 << std::endl;
+    } else if (input_01 < input_02) {
+      std::cout << "The smaller value is: " << input_01 << std::endl;
+      std::cout << "The larger value is: " << input_02 << std::endl;
+    }
   }
   return 0;
 }

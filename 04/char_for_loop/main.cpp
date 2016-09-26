92662e5e7e76855e2f780efe3864d77a71902647
David Newman <david.r.niciforovic@gmail.com>
2016-09-26 10:23:02 -0700

    chore: format code

--- 04/char_for_loop/main.cpp
+++ 04/char_for_loop/main.cpp
@@ -5,15 +5,31 @@
 
 int main() {
 
-    int i = 0;
+    constexpr int alphabet_length = 25;
 
-    constexpr int limit = 30;
+    constexpr int lowercase_starting_index = 0;
 
-    for(int i = 0; i <= limit; i++) {
+    constexpr int uppercase_starting_index = 224;
+
+    std::cout << "\nPrinting lowercase characters [a - z] : \n"
+              << std::endl;
+
+    for(int i = lowercase_starting_index; i <= alphabet_length; i++) {
          std::cout << char('a' + i) << "\t" << i << std::endl;
     }
 
-    for(int i = 224; i <= 224 + limit; i++) {
+    std::cout << "\nPrinting uppercase characters [A - Z] : \n"
+              << std::endl;
+
+    for(int i = uppercase_starting_index;
+            i <= uppercase_starting_index + alphabet_length;
+            i++) {
+        std::cout << char('a' + i) << "\t" << i << std::endl;
+    }
+
+    std::cout << "\nPrinting standard digits [0 - 9] : \n" << std::endl;
+
+    for(int i = 207; i <= 216; i++) {
         std::cout << char('a' + i) << "\t" << i << std::endl;
     }
 }

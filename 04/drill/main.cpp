570ae22bbc7ea5cfc707e1035aae7c004c607a72
David Newman <david.r.niciforovic@gmail.com>
2016-09-26 14:43:18 -0700

    refactor: use `int`s rather than `string`s

--- 04/drill/main.cpp
+++ 04/drill/main.cpp
@@ -6,12 +6,11 @@ int main() {
   std::cout << "Please enter two integers (CMD + D or | to terminate):"
             << std::endl;
 
-  std::string input_01;
-  std::string input_02;
+  int input_01;
+  int input_02;
 
   while((std::cin >> input_01 && std::cin >> input_02)
-            && (input_01 != std::string("|")
-                && input_02 != std::string("|"))) {
+            && (input_01 != '|') && input_02 != '|') {
     if(input_01 > input_02) {
       std::cout << "The smaller value is: " << input_02 << std::endl;
       std::cout << "The larger value is: " << input_01 << std::endl;
